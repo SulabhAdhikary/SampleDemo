c65891d35f97056883ad88fd4a4cce762e035db7
SulabhAdhikary <sulabhadhikary@gmail.com>
2018-03-19 22:39:55 -0500

    Fixed unit test for controller
    Fixed unit test for controller

--- UnitTestWebLayer/Unittest.cs
+++ UnitTestWebLayer/Unittest.cs
@@ -12,30 +12,44 @@ using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using DataContext.RepositoryImplementation;
 
 namespace UnitTestWebLayer
 {
     [TestClass]
     public class Unittest
     {
-        [TestMethod]
-        public async Task TestMethod1()
-        {
-           var rep= new Mock<IRepositoryBase>();
+        private DataContext.DataContext _dataContext;
 
+        //Unittest()
+        //{
+          
+        //}
+
+        public void InitContext()
+        {
+            var builder = new DbContextOptionsBuilder<DataContext.DataContext>()
+                .UseInMemoryDatabase("NorthWindLocalContext");
+
+            var context = new DataContext.DataContext(builder.Options);
             var students = new List<Student>
             {
                 new Student{ ID=1,FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
                 new Student{ ID=2,FirstMidName="Meredith",LastName="Alonso",EnrollmentDate=DateTime.Parse("2002-09-01")},
 
-            }.AsQueryable<Student>();
+            };
+            context.Students.AddRange(students);
+            int changed = context.SaveChanges();
+            _dataContext = context;
+        }
 
-          
 
-
-            rep.Setup(t => t.GetAll<Student>()).Returns(students);
-            FetchDataController objController = new FetchDataController(rep.Object);
-
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            InitContext();
+            var rep= new Repository(_dataContext);
+           FetchDataController objController = new FetchDataController(rep);
             var tskStudent = await objController.GetAllStudents();
             
             Assert.AreEqual(tskStudent.Count, 2);
